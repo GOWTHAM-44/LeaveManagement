5918b0e71d8a63ecd60a0be00df6490906b68165
GOWTHAM-44 <gowthamtharani0408@gmail.com>
2025-09-14 15:17:11 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/example/LeaveManagament/LeaveManagamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeaveManagament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeaveManagament/LeaveManagamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeaveManagament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
